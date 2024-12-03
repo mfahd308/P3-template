bcdda17b8fd3dd61b8dfb1bd8b322e74fcc1f657
paapa-kusi <166747979+paapa-kusi@users.noreply.github.com>
2024-12-03 00:21:55 -0500

    merged all branches and features

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def get_game_data(access_token, limit=500, offset=0):
 # Main function to fetch and process game data
 def main():
     access_token = auth()
-    maxGames = 250
+    maxGames = 25000
     limit = 500
     offset = 0
 
